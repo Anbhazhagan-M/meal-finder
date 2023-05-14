c62f80bddd2653d0f9bf1dc3072be195623c5339
Anbhazhagan-M <133607510+Anbhazhagan-M@users.noreply.github.com>
2023-05-14 16:45:58 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,16 +18,16 @@ module.exports = {
     'react',
     'react-hooks',
   ],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/prop-types': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'max-len': 0,
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off',
-  }
+//   rules: {
+//     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+//     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+//     'react/react-in-jsx-scope': 0,
+//     'react/jsx-filename-extension': 0,
+//     'react/jsx-props-no-spreading': 0,
+//     'react/prop-types': 0,
+//     'jsx-a11y/anchor-is-valid': 0,
+//     'max-len': 0,
+//     'react/no-unescaped-entities': 'off',
+//     '@next/next/no-page-custom-font': 'off',
+//   }
 };
